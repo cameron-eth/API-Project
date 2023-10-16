85e96e03caad46409374fe025bc9d0a32b58a545
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-10-16 16:32:03 -0700

    1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,11 @@
+const { restoreUser } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
 const router = require('express').Router();
+router.use(restoreUser);
+
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
@@ -7,6 +14,41 @@ router.post('/test', function(req, res) {
 
 
 
+// GET /api/set-token-cookie
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+
+// GET /api/restore-user
+
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+
+router.use(restoreUser);
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 
 
