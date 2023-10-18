56968c12a93a9a73c28d0409f115c97dc9471254
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-10-18 15:54:30 -0700

    2

--- backend/db/migrations/20231018211338-add-first-last-name-to-users.js
+++ backend/db/migrations/20231018211338-add-first-last-name-to-users.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    options.tableName = 'Users'
+    options.tableName = 'Users';
     await queryInterface.addColumn(options, 'first_name', {
       type: Sequelize.STRING(255), // You can adjust the length as needed
       allowNull: false
@@ -21,7 +21,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Users'
+    options.tableName = 'Users';
     await queryInterface.removeColumn(options, 'first_name');
     await queryInterface.removeColumn(options, 'last_name');
   }
