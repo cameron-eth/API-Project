3d1f6734bd2592017a8ea8dc47dfe73c9f100397
Cameron Norfleet <cameron@camerons-mbp.lan>
2023-11-02 08:57:49 -0700

    2

--- backend/routes/api/spot_images.js
+++ backend/routes/api/spot_images.js
@@ -7,10 +7,9 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
   const { user } = req;
   const imageId = req.params.imageId;
 
-  // Validate the "imageId" as a valid integer
-  // if (!Number.isInteger(imageId) || imageId === null) {
-  //   return res.status(400).json({ message: 'Forbidden' });
-  // }
+  if (!Number.isInteger(imageId) || imageId === null) {
+    return res.status(400).json({ message: 'Forbidden' });
+  }
 
   try {
     // Find the Spot Image by ID to get the spotId
