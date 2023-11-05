4eb02728edb81518b666a876e22081278c48c7d7
Cameron Norfleet <cameron@camerons-mbp.lan>
2023-11-05 13:59:55 -0800

    1

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -36,18 +36,40 @@ router.get('/', async (req, res) => {
     where.price = { ...where.price, [Sequelize.Op.lte]: maxPrice }; // Less than or equal to maxPrice
   }
 
-  // Validation code (preserved from before)
   const errors = {};
 
   if (isNaN(page) || page < 1 || page > 10) {
     errors.page = 'Page must be between 1 and 10';
   }
-
+  
   if (isNaN(size) || size < 1 || size > 20) {
     errors.size = 'Size must be between 1 and 20';
   }
+  
+  if (minLat && (isNaN(minLat) || minLat < -90 || minLat > 90)) {
+    errors.minLat = 'Minimum latitude is invalid';
+  }
+  
+  if (maxLat && (isNaN(maxLat) || maxLat < -90 || maxLat > 90)) {
+    errors.maxLat = 'Maximum latitude is invalid';
+  }
+  
+  if (minLng && (isNaN(minLng) || minLng < -180 || minLng > 180)) {
+    errors.minLng = 'Minimum longitude is invalid';
+  }
+  
+  if (maxLng && (isNaN(maxLng) || maxLng < -180 || maxLng > 180)) {
+    errors.maxLng = 'Maximum longitude is invalid';
+  }
+  
+  if (minPrice && (isNaN(minPrice) || minPrice < 0)) {
+    errors.minPrice = 'Minimum price must be greater than or equal to 0';
+  }
+  
+  if (maxPrice && (isNaN(maxPrice) || maxPrice < 0)) {
+    errors.maxPrice = 'Maximum price must be greater than or equal to 0';
+  }
 
-  // ... (Validation code remains the same)
 
   if (Object.keys(errors).length > 0) {
     return res.status(400).json({
